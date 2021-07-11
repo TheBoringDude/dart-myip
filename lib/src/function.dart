ea44952624616b67e15e709d810080e95422bbf0
TheBoringDude <iamcoderx@gmail.com>
2021-07-11 22:34:57 +0800

    feat: add `[server]` as params to main function for allowing to use of personally configured servers

--- lib/src/function.dart
+++ lib/src/function.dart
@@ -2,8 +2,9 @@ import 'package:dart_myip/dart_myip.dart';
 import 'package:dart_myip/src/request.dart';
 
 /// getMyIP handles the client connection to the API. It returns a ClientIP instance.
-Future<ClientIP> getMyIP() async {
-  final r = await RequestHandler.handleRequest();
+/// If you have configured your own server, you can pass a parameter to the function with [server] as String.
+Future<ClientIP> getMyIP({String? server}) async {
+  final r = await RequestHandler.handleRequest(altUrl: server);
 
   return ClientIP.fromJson(r);
 }
--- lib/src/request.dart
+++ lib/src/request.dart
@@ -4,8 +4,10 @@ import 'package:dart_myip/src/errors.dart';
 import 'package:http/http.dart' as http;
 
 class RequestHandler {
-  static Future<Map<String, dynamic>> handleRequest() async {
-    final response = await http.get(Uri.parse('https://my-ip.theboringdude.workers.dev/'));
+  static Future<Map<String, dynamic>> handleRequest({String? altUrl}) async {
+    // If [altUrl] is not null, use it, otherwise use the default server.
+    final response =
+        await http.get(Uri.parse(altUrl ?? 'https://my-ip.theboringdude.workers.dev/'));
 
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
